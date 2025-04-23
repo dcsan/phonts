bcb0c4107a2fe883240cb61882310c50630c0d88
DC <dc@rik.ai>
2025-04-22 20:19:32 -0400

    add blends

--- sketch.js
+++ sketch.js
@@ -29,46 +29,51 @@ function draw() {
   background(220);
 
   const baseDX = windowWidth * windowHeight;
+  // blendMode(ADD);
 
-  for (let x = 0; x < cfg.imgWidth; x++) {
-    for (let y = 0; y < cfg.imgHeight; y++) {
-      // screen coords
-      const sx = x * cfg.scaleUp;
-      const sy = y * cfg.scaleUp;
+  for (let i = 0; i < 100; i++) {
+    for (let x = 0; x < cfg.imgWidth; x++) {
+      for (let y = 0; y < cfg.imgHeight; y++) {
+        if (i % 100 == 0) {
+          blendMode(ADD);
+        } else {
+          blendMode(BLEND);
+        }
 
-      const mouseDist = dist(mouseX, mouseY, sx, sy);
-      const imgSize = (500 - mouseDist) / 2;
-      // const mouseRatio = mouseDist / baseDX;
-      // const imgSize = mouseRatio * cfg.dotSize;
+        // screen coords
+        const sx = x * cfg.scaleUp;
+        const sy = y * cfg.scaleUp;
 
-      if (count == 200) {
-        console.log({ mouseDist, imgSize, x, y });
+        const mouseDist = dist(mouseX, mouseY, sx, sy);
+        const imgSize = (500 - mouseDist) / 2;
+        // const mouseRatio = mouseDist / baseDX;
+        // const imgSize = mouseRatio * cfg.dotSize;
+
+        if (count == 200) {
+          console.log({ mouseDist, imgSize, x, y });
+        }
+
+        // console.log({ dcalc });
+        // let imgSize = map(mouseX, 0, width, 0, cfg.maxDotSize) * dist;
+
+        stroke(0, 0, 0, 5);
+        count++;
+        const pixel = img.get(x, y);
+        const brt = brightness(pixel);
+
+        push();
+        translate(sx, sy);
+
+        if (brt > 50) {
+          fill(128, 0, 0, 50);
+        } else {
+          fill(0, 128, 0, 50);
+        }
+        ellipse(0, 0, imgSize, imgSize);
+        pop();
       }
-
-      // console.log({ dcalc });
-      // let imgSize = map(mouseX, 0, width, 0, cfg.maxDotSize) * dist;
-
-      stroke(0, 0, 0, 5);
-      count++;
-      const pixel = img.get(x, y);
-      const brt = brightness(pixel);
-
-      push();
-      translate(sx, sy);
-
-      if (brt > 50) {
-        fill(128, 0, 0, 50);
-      } else {
-        fill(0, 128, 0, 50);
-      }
-      ellipse(0, 0, imgSize, imgSize);
-      pop();
     }
   }
-
-  // rect(100, 100, 200, 200);
-  // fill(255, 0, 0);
-  // rect(100, 100, 200, 200);
 
   // image(img, 50, 50, 12, 12);
 }
